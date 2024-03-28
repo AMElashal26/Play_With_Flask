6a2037ccd4e7dcef358017dde12996f14cf922c4
Your Name <you@example.com>
2024-03-27 23:24:10 -0400

    handle form in flask

--- fla.py
+++ fla.py
@@ -1,11 +1,20 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
 @app.route('/hello/<name>')
 def hello_name(name):
     return render_template('hello.html', name=name)
+
+
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    username = request.form['username']
+    return f'Hello, {username}!'
+
 
 @app.route('/')
 def hello_flask():
